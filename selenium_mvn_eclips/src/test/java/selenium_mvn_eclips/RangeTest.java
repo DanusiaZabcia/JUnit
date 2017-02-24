e005508bf81c647222b36569a8dfc6dde4a0baed
Danusia Żabińska <danusiazabcia@gmail.com>
2017-02-24 14:46:44 +0100

    working JUnit Test

---
+++ selenium_mvn_eclips/src/test/java/selenium_mvn_eclips/RangeTest.java
@@ -0,0 +1,18 @@
+package selenium_mvn_eclips;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RangeTest {
+
+@Test
+	
+	public void shouldSayIs15InRange()
+	{
+		Range range= new Range(10,20);
+	assertTrue("nie jest", range.isInRange(34));
+		
+	}
+
+}
--- selenium_mvn_eclips/src/test/java/selenium_mvn_eclips/TestJuni.java
+++
@@ -1,5 +0,0 @@
-package selenium_mvn_eclips;
-
-public class TestJuni {
-
-}
