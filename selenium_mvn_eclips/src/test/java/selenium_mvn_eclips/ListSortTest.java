5359cd2cd860126db01d651da2e35f153acc8d9f
Danusia Żabińska <danuta.zabinska@ebicom.pl>
2017-02-24 16:02:59 +0100

    to work with

--- selenium_mvn_eclips/src/test/java/selenium_mvn_eclips/ListSortTest.java
+++ selenium_mvn_eclips/src/test/java/selenium_mvn_eclips/ListSortTest.java
@@ -1,13 +1,31 @@
 package selenium_mvn_eclips;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class ListSortTest {
 
+	@Before
+	
+	public void lista(){
+		
+		Random mix = new Random();
+		
+	for (int i = 0; i < 30; i++) {
+		ArrayList<Integer> list= new ArrayList<Integer>();
+		Integer a = mix.nextInt(898);
+		list.add(a);
+	}
+	}
+	
 	@Test
-	public void test() {
+	public void isSortIsGood() {
+		
 		fail("Not yet implemented");
 	}
 
