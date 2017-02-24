4be83664e680aec1a4e7e98f2307bbbf26551116
Danusia Żabińska <danuta.zabinska@ebicom.pl>
2017-02-24 15:31:37 +0100

    To create JUnit tests

---
+++ selenium_mvn_eclips/src/main/java/selenium_mvn_eclips/ListSort.java
@@ -0,0 +1,37 @@
+package selenium_mvn_eclips;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ListSort {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		Random mix = new Random();
+
+		for (int i = 0; i < 30; i++) {
+			Integer a = mix.nextInt(898);
+			list.add(a);
+
+		}
+
+		if (list.size() > 0) {
+			for (int x = 0; x < list.size() - 1; x++) {
+				for (int i = 0; i < list.size() - 1; i++) {
+
+					if (list.get(i).compareTo(list.get(i + 1)) > 0) {
+						Integer temp;
+						temp = list.get(i);
+						list.set(i, list.get(i + 1));
+						list.set(i + 1, temp);
+					}
+				}
+			}
+		}
+
+		System.out.println( "Posortowane" + list);
+
+	}
+
+}
---
+++ selenium_mvn_eclips/src/test/java/selenium_mvn_eclips/ListSortTest.java
@@ -0,0 +1,14 @@
+package selenium_mvn_eclips;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ListSortTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
