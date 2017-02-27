0046d03883495c32f5b3b91244ad6b0299079032
Danusia Żabińska <danusiazabcia@gmail.com>
2017-02-27 11:50:03 +0100

    Tests are working good

--- selenium_mvn_eclips/src/test/java/selenium_mvn_eclips/ListSortTest.java
+++ selenium_mvn_eclips/src/test/java/selenium_mvn_eclips/ListSortTest.java
@@ -1,8 +1,6 @@
 package selenium_mvn_eclips;
 
-
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,26 +11,52 @@ import org.junit.Test;
 public class ListSortTest {
 
 	@Test
-	
-	public void isSortingdWorks(){
-		
+
+	public void isSortingWorksAsSortMethod() {
+
 		Random mix = new Random();
-		
-	for (int i = 0; i < 30; i++) {
-		ArrayList<Integer> list= new ArrayList<Integer>();
-		Integer a = mix.nextInt(20);
-		list.add(a);
-		ListSort newlist= new ListSort(list);
-	
-		
-		
-		
-		
-		 Collections.sort(list);
-		
-		assertEquals( list, newlist.bubbleSort());
-		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < 30; i++) {
+			Integer a = mix.nextInt(20);
+			list.add(a);
+		}
+		ListSort newlist = new ListSort(list);
+
+		Collections.sort(list);
+
+		assertEquals(list, newlist.bubbleSort());
+
 	}
+
+	@Test
+	public void isSortingWorksWith0() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < 30; i++) {
+			Integer a = 0;
+			list.add(a);
+		}
+		ListSort newlist = new ListSort(list);
+		newlist.bubbleSort();
+	}
+
+	@Test
+	public void isSortingWorksWithSize200() {
+		Random mix = new Random();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < 200; i++) {
+			Integer a = mix.nextInt(20);
+			list.add(a);
+		}
+		ListSort newlist = new ListSort(list);
+		assertEquals(list, newlist.bubbleSort());
+	}
+
+	@Test(expected = java.lang.Error.class)
+	public void doSortingWillFall() {
+		ArrayList<String> list = new ArrayList<String>();
+
+		ListSort newlist = new ListSort(list);
+
 	}
 
 }
