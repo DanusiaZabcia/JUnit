3e26cebb0d55efa03167fac4760d99f46bce278f
Danusia Żabińska <danusiazabcia@gmail.com>
2017-02-28 15:54:29 +0100

    Problem with "isSortingBWorksWithSize200" Test

--- selenium_mvn_eclips/src/test/java/selenium_mvn_eclips/ListSortTest.java
+++ selenium_mvn_eclips/src/test/java/selenium_mvn_eclips/ListSortTest.java
@@ -1,21 +1,23 @@
 package selenium_mvn_eclips;
 
+import static org.junit.Assert.assertThat;
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 public class ListSortTest {
 
 	@Test
 
 	public void isSortingBWorksAsSortMethod() {
 
-		
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		ArrayList<Integer> list2 = new ArrayList<Integer>();
-		
+
 		list.add(234);
 		list.add(4343);
 		list.add(43);
@@ -23,7 +25,7 @@ public class ListSortTest {
 		list.add(455311);
 		list.add(-3);
 		list.add(8);
-		
+
 		list2.add(234);
 		list2.add(4343);
 		list2.add(43);
@@ -31,24 +33,19 @@ public class ListSortTest {
 		list2.add(455311);
 		list2.add(-3);
 		list2.add(8);
-		
-		
-		
+
 		ListSort newlist = new ListSort(list);
 
 		Collections.sort(list2);
 		System.out.println(list2);
 		System.out.println(newlist.bubbleSort());
-		assertThat(newlist.bubbleSort(),
-				IsIterableContainingInOrder.contains(list2.toArray()));
-		
+		assertThat(newlist.bubbleSort(), IsIterableContainingInOrder.contains(list2.toArray()));
 
 	}
-	
-	@Test
-public void isSortingChoiceWorksAsSortMethod() {
 
-		
+	@Test
+	public void isSortingChoiceWorksAsSortMethod() {
+
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		ArrayList<Integer> list2 = new ArrayList<Integer>();
 		list.add(234);
@@ -58,7 +55,7 @@ public void isSortingChoiceWorksAsSortMethod() {
 		list.add(455311);
 		list.add(-3);
 		list.add(8);
-		
+
 		list2.add(234);
 		list2.add(4343);
 		list2.add(43);
@@ -66,17 +63,17 @@ public void isSortingChoiceWorksAsSortMethod() {
 		list2.add(455311);
 		list2.add(-3);
 		list2.add(8);
-		
+
 		ListSort newlist = new ListSort(list);
 
 		Collections.sort(list2);
 		System.out.println(list2);
 		System.out.println(newlist.sortByChoice());
-		assertThat(newlist.sortByChoice(),
-				IsIterableContainingInOrder.contains(list2.toArray()));
-		
+		assertThat(newlist.sortByChoice(), IsIterableContainingInOrder.contains(list2.toArray()));
+
 	}
-	/*@Test
+
+	@Test
 	public void isSortingBWorksWith0() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		ArrayList<Integer> list2 = new ArrayList<Integer>();
@@ -87,10 +84,13 @@ public void isSortingChoiceWorksAsSortMethod() {
 		}
 		ListSort newlist = new ListSort(list);
 		Collections.sort(list2);
-		assertEquals(list2, newlist.bubbleSort());
-	}*/
+		System.out.println(list2);
+		System.out.println(newlist.bubbleSort());
+		// assertEquals(list2, newlist.bubbleSort());
+		assertThat(newlist.bubbleSort(), IsIterableContainingInOrder.contains(list2.toArray()));
+	}
 
-	/*@Test
+	@Test
 	public void isSortingBWorksWithSize200() {
 		Random mix = new Random();
 		ArrayList<Integer> list = new ArrayList<Integer>();
@@ -99,13 +99,16 @@ public void isSortingChoiceWorksAsSortMethod() {
 			Integer a = mix.nextInt(20);
 			list.add(a);
 			list2.add(a);
-			
+
 		}
 		ListSort newlist = new ListSort(list);
-		assertEquals(list2, newlist.bubbleSort());
-		//assertTrue( list.retainAll( newlist.bubbleSort()));
+		// assertEquals(list2, newlist.bubbleSort());
+		// assertTrue( list.retainAll( newlist.bubbleSort()));
+		System.out.println(list2);
+		System.out.println(newlist.bubbleSort());
+		assertThat(newlist.bubbleSort(), IsIterableContainingInOrder.contains(list2.toArray()));
 		
-		
+
 	}
 
 	@Test(expected = java.lang.Error.class)
@@ -114,53 +117,47 @@ public void isSortingChoiceWorksAsSortMethod() {
 
 		ListSort newlist = new ListSort(list);
 
-	}*/
+	}
 
-	/*@Test
+	/*
+	 * @Test
+	 * 
+	 * public void isSortingChWorksAsSortMethod() {
+	 * 
+	 * Random mix = new Random(); ArrayList<Integer> list = new
+	 * ArrayList<Integer>(); ArrayList<Integer> list2 = new
+	 * ArrayList<Integer>(); for (int i = 0; i < 30; i++) { Integer a =
+	 * mix.nextInt(200); list.add(a); list2.add(a); } ListSort newlist = new
+	 * ListSort(list);
+	 * 
+	 * Collections.sort(list2);
+	 * 
+	 * //assertEquals(list2, newlist.sortByChoice()); //assertTrue(
+	 * list.retainAll( newlist.sortByChoice()));
+	 * assertThat(newlist.sortByChoiceDecent(),
+	 * IsIterableContainingInOrder.contains(list2.toArray()));
+	 * 
+	 * }
+	 */
 
-	public void isSortingChWorksAsSortMethod() {
-
-		Random mix = new Random();
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		ArrayList<Integer> list2 = new ArrayList<Integer>();
-		for (int i = 0; i < 30; i++) {
-			Integer a = mix.nextInt(200);
-			list.add(a);
-			list2.add(a);
-		}
-		ListSort newlist = new ListSort(list);
-
-		Collections.sort(list2);
-
-		//assertEquals(list2, newlist.sortByChoice());
-		//assertTrue( list.retainAll( newlist.sortByChoice()));
-		assertThat(newlist.sortByChoiceDecent(),
-				IsIterableContainingInOrder.contains(list2.toArray()));
-
-	}*/
-	
-	/*@Test
-
-	public void isSortingChDecentWorksAsSortMethod() {
-
-		Random mix = new Random();
-		final ArrayList<Integer> list = new ArrayList<Integer>();
-		 ArrayList<Integer> list2 = new ArrayList<Integer>();
-		for (int i = 0; i < 30; i++) {
-			Integer a = mix.nextInt(200);
-			list.add(a);
-			
-			list2.add(a);
-		}
-		ListSort newlist = new ListSort(list);
-		
-
-		//assertFalse(list.containsAll(newlist.sortByChoiceDecent())&&newlist.sortByChoiceDecent().containsAll(list));
-		//assertTrue( list.retainAll( newlist.sortByChoiceDecent()));
-		//System.out.println(list);
-		System.out.println(newlist.sortByChoiceDecent());
-		System.out.println(list2);
-		assertThat(newlist.sortByChoiceDecent(),
-				IsIterableContainingInOrder.contains(list2.toArray()));
-	}*/
+	/*
+	 * @Test
+	 * 
+	 * public void isSortingChDecentWorksAsSortMethod() {
+	 * 
+	 * Random mix = new Random(); final ArrayList<Integer> list = new
+	 * ArrayList<Integer>(); ArrayList<Integer> list2 = new
+	 * ArrayList<Integer>(); for (int i = 0; i < 30; i++) { Integer a =
+	 * mix.nextInt(200); list.add(a);
+	 * 
+	 * list2.add(a); } ListSort newlist = new ListSort(list);
+	 * 
+	 * 
+	 * //assertFalse(list.containsAll(newlist.sortByChoiceDecent())&&newlist.
+	 * sortByChoiceDecent().containsAll(list)); //assertTrue( list.retainAll(
+	 * newlist.sortByChoiceDecent())); //System.out.println(list);
+	 * System.out.println(newlist.sortByChoiceDecent());
+	 * System.out.println(list2); assertThat(newlist.sortByChoiceDecent(),
+	 * IsIterableContainingInOrder.contains(list2.toArray())); }
+	 */
 }
